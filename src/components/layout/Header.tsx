765819a114b105afd6f54faeec279ffe4d6600a0
Dzmitry Drevich <jeidan2012@gmail.com>
2025-07-13 16:34:25 +0200

    refactor: update layout components (class conversion)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,29 +1,31 @@
 import logo from '@/assets/logo-fox.svg';
-import type { JSX } from 'react';
+import React from 'react';
 
-const Header = (): JSX.Element => {
-  return (
-    <header className="text-black dark:text-white body-font w-full">
-      <div className="mx-auto flex flex-wrap py-5 flex-col md:flex-row items-center">
-        <a
-          href="#"
-          className="flex gap-2 title-font font-medium items-center md:justify-start justify-center "
-        >
-          <img src={logo} alt={logo} width={50} height={50} />
-          <h1 className="text-3xl">Jikan</h1>
-        </a>
-        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-          <a className="mr-5 hover:text-gray-900"></a>
-          <a className="mr-5 hover:text-gray-900"></a>
-          <a className="mr-5 hover:text-gray-900"></a>
-          <a className="mr-5 hover:text-gray-900"></a>
-        </nav>
-        <button className="inline-flex items-center bg-gray-100 text-black border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 hover:cursor-pointer rounded text-base mt-4 md:mt-0">
-          Button
-        </button>
-      </div>
-    </header>
-  );
-};
+class Header extends React.Component {
+  render(): React.JSX.Element {
+    return (
+      <header className="text-black dark:text-white body-font w-full">
+        <div className="mx-auto flex flex-wrap py-5 flex-col md:flex-row items-center">
+          <a
+            href="#"
+            className="flex gap-2 title-font font-medium items-center md:justify-start justify-center "
+          >
+            <img src={logo} alt={logo} width={50} height={50} />
+            <h1 className="text-3xl">Jikan</h1>
+          </a>
+          <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+            <a className="mr-5 hover:text-gray-900"></a>
+            <a className="mr-5 hover:text-gray-900"></a>
+            <a className="mr-5 hover:text-gray-900"></a>
+            <a className="mr-5 hover:text-gray-900"></a>
+          </nav>
+          <button className="inline-flex items-center bg-gray-100 text-black border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 hover:cursor-pointer rounded text-base mt-4 md:mt-0">
+            Button
+          </button>
+        </div>
+      </header>
+    );
+  }
+}
 
 export default Header;
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,16 +1,22 @@
-import type { JSX } from 'react';
 import type { LayoutProps } from '@/types/types';
 import Header from './Header';
 import Footer from './Footer';
+import React from 'react';
 
-const Layout = ({ children }: LayoutProps): JSX.Element => {
-  return (
-    <div className="container flex flex-col items-center justify-center text-center mx-auto light:bg-gray-100 text-black dark:text-white h-dvh">
-      <Header />
-      <main className="grow flex flex-col justify-start items-center w-full">{children}</main>
-      <Footer />
-    </div>
-  );
-};
+class Layout extends React.Component<LayoutProps> {
+  render(): React.JSX.Element {
+    const { children } = this.props;
+
+    return (
+      <div className="container flex flex-col items-center justify-center text-center mx-auto light:bg-gray-100 text-black dark:text-white min-h-full">
+        <Header />
+        <main className="grow gap-10 flex flex-col justify-start items-center w-full">
+          {children}
+        </main>
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default Layout;
