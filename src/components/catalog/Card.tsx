c6a9b6e7669558718986dfd7c268051f98d9573f
Dzmitry Drevich <jeidan2012@gmail.com>
2025-07-13 16:48:12 +0200

    refactor: move Card & CardList to catalog-section and convert to classes

---
+++ src/components/catalog/Card.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import type { CardProps } from '@/types/types';
+import { StarIcon } from 'lucide-react';
+
+class Card extends React.Component<CardProps> {
+  render(): React.JSX.Element {
+    const { title, year, images, score } = this.props.item;
+
+    return (
+      <a href="#" className="hover:scale-102 duration-300">
+        <div className="flex flex-col border-1 rounded-xl overflow-clip relative h-full">
+          <div className="aspect-[2/3] relative">
+            <img
+              alt={title}
+              loading="lazy"
+              src={images.webp.large_image_url}
+              className="absolute w-full h-full object-cover"
+            />
+          </div>
+          <div className="flex flex-col grow p-3 self-start justify-between text-left">
+            <h3 className="font-medium text-sm line-clamp-2">{title}</h3>
+            <p className="text-xs text-muted-foreground">{year ?? 'TBD'}</p>
+          </div>
+          <div className="absolute top-2 right-2 bg-black/70 text-white px-2 py-1 rounded-md text-xs flex items-center">
+            <StarIcon className="h-3 w-3 mr-1 text-yellow-400 fill-yellow-400"></StarIcon>
+            <span>{score}</span>
+          </div>
+        </div>
+      </a>
+    );
+  }
+}
+
+export default Card;
