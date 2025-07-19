cd956378710fb367a74b0be542b303fd640f9eda
Dzmitry Drevich <jeidan2012@gmail.com>
2025-07-19 18:08:27 +0200

    chore: install and configure jest for testing

---
+++ jest.config.ts
@@ -0,0 +1,41 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['ts-jest', { tsconfig: './tsconfig.jest.json' }]
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testMatch: [
+    '<rootDir>/tests/**/*.{test,spec}.{ts,tsx}',
+    '<rootDir>/src/**/*.{test,spec}.{ts,tsx}'
+  ],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  collectCoverage: true,
+  coverageReporters: ['html', 'lcov'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/index.{ts,tsx}',
+    '!src/**/*.test.{ts,tsx}',
+    '!src/**/*.spec.{ts,tsx}',
+    '!src/setupTests.{ts}',
+    '!src/main.tsx',
+    '!src/types/**'
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 50,
+      functions: 50,
+      lines: 50
+    }
+  }
+};
+
+export default config;
