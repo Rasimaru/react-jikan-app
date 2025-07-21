6925654b0072ebdb048b101299d7b688f980c2f6
Dzmitry Drevich <jeidan2012@gmail.com>
2025-07-21 02:37:44 +0200

    refactor: convert hero-section components to functional

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -2,18 +2,17 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import type { HeroProps } from '@/types/types';
 
-class Hero extends React.Component<HeroProps> {
-  render(): React.JSX.Element {
-    return (
-      <section className="hero flex items-center justify-center flex-col gap-6  w-full">
-        <h2 className="text-5xl">Discover Your Next Adventure</h2>
-        <p className="text-lg text-center">
-          Track, discover, and discuss your favorite anime and manga all in one place.
-        </p>
-        <SearchBar onSearch={this.props.onSearch} searchQuery={this.props.searchQuery}></SearchBar>
-      </section>
-    );
-  }
-}
+const Hero = (props: HeroProps): React.JSX.Element => {
+  const { onSearch, searchQuery } = props;
+  return (
+    <section className="hero flex items-center justify-center flex-col gap-6  w-full">
+      <h2 className="text-5xl">Discover Your Next Adventure</h2>
+      <p className="text-lg text-center">
+        Track, discover, and discuss your favorite anime and manga all in one place.
+      </p>
+      <SearchBar onSearch={onSearch} searchQuery={searchQuery}></SearchBar>
+    </section>
+  );
+};
 
 export default Hero;
--- src/components/hero/SearchBar.tsx
+++ src/components/hero/SearchBar.tsx
@@ -1,44 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import type { SearchBarProps } from '@/types/types';
 import { Search } from 'lucide-react';
 
-class SearchBar extends React.Component<SearchBarProps> {
-  private tempQuery = this.props.searchQuery;
+const SearchBar = (props: SearchBarProps) => {
+  const { onSearch, searchQuery } = props;
+  const [query, setQuery] = useState(searchQuery);
 
-  handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    this.tempQuery = e.target.value;
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setQuery(e.target.value);
   };
 
-  handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
-    const query = this.tempQuery.trim();
-    localStorage.setItem('searchQuery', query);
-    this.props.onSearch(query);
+    const trimmedQuery = query.trim();
+    localStorage.setItem('searchQuery', trimmedQuery);
+    onSearch(trimmedQuery);
   };
 
-  render(): React.JSX.Element {
-    return (
-      <form
-        onSubmit={this.handleSubmit}
-        className="flex sm:w-[500px] transition-all duration-700 delay-500 translate-y-0 opacity-100 relative"
+  return (
+    <form
+      role="form"
+      onSubmit={handleSubmit}
+      className="flex sm:w-[500px] transition-all duration-700 delay-500 translate-y-0 opacity-100 relative"
+    >
+      <Search className="absolute top-1/4 left-2"></Search>
+      <input
+        role="searchbox"
+        type="search"
+        defaultValue={query}
+        onChange={handleChange}
+        placeholder="Search for anime or manga..."
+        className="flex grow rounded-md border border-input bg-background px-3 py-2 text-base md:text-lg pl-10 h-12 rounded-r-none"
+      ></input>
+      <button
+        type="submit"
+        className="inline-flex items-center bg-neutral-900 text-white dark:bg-gray-100 dark:text-black border-0 py-1 px-3 focus:outline-none hover:bg-neutral-700 dark:hover:bg-gray-300 hover:cursor-pointer rounded-r text-base duration-300 font-semibold"
       >
-        <Search className="absolute top-1/4 left-2"></Search>
-        <input
-          type="search"
-          defaultValue={this.props.searchQuery}
-          onChange={this.handleChange}
-          placeholder="Search for anime or manga..."
-          className="flex grow rounded-md border border-input bg-background px-3 py-2 text-base md:text-lg pl-10 h-12 rounded-r-none"
-        ></input>
-        <button
-          type="submit"
-          className="inline-flex items-center bg-neutral-900 text-white dark:bg-gray-100 dark:text-black border-0 py-1 px-3 focus:outline-none hover:bg-neutral-700 dark:hover:bg-gray-300 hover:cursor-pointer rounded-r text-base duration-300 font-semibold"
-        >
-          Search
-        </button>
-      </form>
-    );
-  }
-}
+        Search
+      </button>
+    </form>
+  );
+};
 
 export default SearchBar;
