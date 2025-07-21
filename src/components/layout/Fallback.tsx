577c33af025fdb8eb9ddfe699c713acf7c8d4ddd
Dzmitry Drevich <jeidan2012@gmail.com>
2025-07-21 02:36:42 +0200

    refactor: convert layout components to functional

--- src/components/layout/Fallback.tsx
+++ src/components/layout/Fallback.tsx
@@ -2,28 +2,30 @@ import React from 'react';
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 import { BASE_PATH } from '@/types/constants';
 
-class Fallback extends React.Component {
-  render(): React.JSX.Element {
-    return (
-      <section className="container text-center grow flex flex-col justify-center items-center gap-4 h-full">
-        <DotLottieReact
-          src="./Fox-fall.lottie"
-          loop
-          autoplay
-          className="w-50 h-50"
-        ></DotLottieReact>
-        <h2 className="text-4xl">Oops! Sorry for the inconvenience!</h2>
-        <p className="text-2xl">The Fox is already fixing the issue!</p>
-        <a
-          role="button"
-          href={BASE_PATH}
-          className="inline-flex items-center animate-pulse bg-amber-300 text-black border-0 py-2 px-3 focus:outline-none hover:bg-gray-200 hover:cursor-pointer rounded text-[20px] font-bold"
-        >
-          Reload
-        </a>
-      </section>
-    );
-  }
-}
+const Fallback = (): React.JSX.Element => {
+  return (
+    <section
+      role="alert"
+      className="container text-center grow flex flex-col justify-center items-center gap-4 h-full"
+    >
+      <DotLottieReact
+        src="./Fox-fall.lottie"
+        aria-hidden
+        loop
+        autoplay
+        className="w-50 h-50"
+      ></DotLottieReact>
+      <h2 className="text-4xl">Oops! Sorry for the inconvenience!</h2>
+      <p className="text-2xl">The Fox is already fixing the issue!</p>
+      <a
+        role="button"
+        href={BASE_PATH}
+        className="inline-flex items-center animate-pulse bg-amber-300 text-black border-0 py-2 px-3 focus:outline-none hover:bg-gray-200 hover:cursor-pointer rounded text-[20px] font-bold"
+      >
+        Reload
+      </a>
+    </section>
+  );
+};
 
 export default Fallback;
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,33 +1,31 @@
 import React from 'react';
 
-class Footer extends React.Component {
-  render(): React.JSX.Element {
-    return (
-      <footer className="container text-black dark:text-white body-font w-full">
-        <div className="py-5 mx-auto flex items-center justify-center sm:flex-row flex-col">
-          <p className="text-sm sm:ml-6 sm:mt-0 mt-4">
-            <a
-              href="https://rs.school/"
-              rel="noopener noreferrer"
-              className="hover:text-amber-500 duration-300"
-              target="_blank"
-            >
-              © 2025 Rolling Scopes School
-            </a>
-            <span> — </span>
-            <a
-              href="https://github.com/Rasimaru"
-              rel="noopener noreferrer"
-              className="hover:text-amber-500 duration-300"
-              target="_blank"
-            >
-              @Rasimaru
-            </a>
-          </p>
-        </div>
-      </footer>
-    );
-  }
-}
+const Footer = (): React.JSX.Element => {
+  return (
+    <footer className="container text-black dark:text-white body-font w-full">
+      <div className="py-5 mx-auto flex items-center justify-center sm:flex-row flex-col">
+        <p className="text-sm sm:ml-6 sm:mt-0 mt-4">
+          <a
+            href="https://rs.school/"
+            rel="noopener noreferrer"
+            className="hover:text-amber-500 duration-300"
+            target="_blank"
+          >
+            © 2025 Rolling Scopes School
+          </a>
+          <span> — </span>
+          <a
+            href="https://github.com/Rasimaru"
+            rel="noopener noreferrer"
+            className="hover:text-amber-500 duration-300"
+            target="_blank"
+          >
+            @Rasimaru
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
