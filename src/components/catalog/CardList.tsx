1d0c311d150cbd6e3ca07fd91fbbd16887cd69c3
Dzmitry Drevich <jeidan2012@gmail.com>
2025-07-21 02:38:10 +0200

    refactor: convert catalog-section components to functional

--- src/components/catalog/CardList.tsx
+++ src/components/catalog/CardList.tsx
@@ -2,18 +2,16 @@ import React from 'react';
 import type { CardListProps } from '@/types/types';
 import Card from './Card';
 
-class CardList extends React.Component<CardListProps> {
-  render(): React.JSX.Element {
-    const { items } = this.props;
+const CardList = (props: CardListProps): React.JSX.Element => {
+  const { items } = props;
 
-    return (
-      <section className="grid gap-4 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2">
-        {items.map((item, index) => (
-          <Card key={`${item.mal_id}-${index}`} item={item} />
-        ))}
-      </section>
-    );
-  }
-}
+  return (
+    <section className="grid gap-4 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2">
+      {items.map((item, index) => (
+        <Card key={`${item.mal_id}-${index}`} item={item} />
+      ))}
+    </section>
+  );
+};
 
 export default CardList;
