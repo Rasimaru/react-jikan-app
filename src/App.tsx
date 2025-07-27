7de6f890629eddd9865a04c421e8072fd9c6c201
Dzmitry Drevich <jeidan2012@gmail.com>
2025-07-27 15:05:44 +0200

    refactor: convert App component to functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,58 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import Layout from '@/components/layout/Layout';
 import Hero from './components/hero/Hero';
 import CardList from './components/catalog/CardList';
-import type { AppState, JikanApiResponse } from './types/types';
+import type { CardItem, JikanApiResponse } from './types/types';
 import Spinner from './components/layout/Spinner';
 import { API_SEARCH, API_TOP_AIRING } from './types/constants';
 import checkResponse from './services/checkResponse';
 
-class App extends React.Component<object, AppState> {
-  state: AppState = {
-    items: [],
-    searchQuery: '',
-    isLoading: false,
-    error: null
-  };
+const App = () => {
+  const [items, setItems] = useState<CardItem[]>([]);
+  const [searchQuery, setSearchQuery] = useState(() => localStorage.getItem('searchQuery') || '');
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
-  componentDidMount(): void {
-    const query = localStorage.getItem('searchQuery') || '';
+  useEffect(() => {
+    fetchData(searchQuery);
+  }, [searchQuery]);
 
-    if (query === this.state.searchQuery) {
-      this.fetchData(query);
-    } else {
-      this.setState({ searchQuery: query });
-    }
-  }
-
-  componentDidUpdate(_: object, prevState: AppState): void {
-    if (prevState.searchQuery !== this.state.searchQuery) {
-      this.fetchData(this.state.searchQuery);
-    }
-  }
-
-  fetchData(query: string): void {
+  function fetchData(query: string): void {
     const url = query !== '' ? `${API_SEARCH}?q=${encodeURIComponent(query)}` : API_TOP_AIRING;
 
-    this.setState({ isLoading: true, error: null });
+    setIsLoading(true);
+    setError(null);
 
     fetch(url)
       .then(checkResponse<JikanApiResponse>)
-      .then((data) => this.setState({ items: data.data }))
+      .then((data) => setItems(data.data))
       .catch((error: Error) => {
         console.error(error);
-        this.setState({ error: error.message });
+        setError(error.message);
       })
-      .finally(() => this.setState({ isLoading: false }));
+      .finally(() => setIsLoading(false));
   }
 
-  handleSearch = (query: string): void => {
-    this.setState({ searchQuery: query });
+  const handleSearch = (query: string): void => {
+    setSearchQuery(query);
   };
 
-  render(): React.JSX.Element {
-    const { isLoading, error, items, searchQuery } = this.state;
-    const isEmpty = items.length === 0;
-    return (
-      <Layout>
-        <Hero onSearch={this.handleSearch} searchQuery={this.state.searchQuery}></Hero>
-        {isLoading ? (
-          <Spinner />
-        ) : error ? (
-          <p>{error}</p>
-        ) : isEmpty ? (
-          <p className="text-center text-[18px]">
-            Nothing found matching &quot;{searchQuery}&quot;
-          </p>
-        ) : (
-          <CardList items={items} />
-        )}
-      </Layout>
-    );
-  }
-}
+  const isEmpty = items.length === 0;
+
+  return (
+    <Layout>
+      <Hero onSearch={handleSearch} searchQuery={searchQuery}></Hero>
+      {isLoading ? (
+        <Spinner />
+      ) : error ? (
+        <p>{error}</p>
+      ) : isEmpty ? (
+        <p className="text-center text-[18px]">Nothing found matching &quot;{searchQuery}&quot;</p>
+      ) : (
+        <CardList items={items} />
+      )}
+    </Layout>
+  );
+};
 
 export default App;
