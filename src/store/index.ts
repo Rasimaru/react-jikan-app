91bd6a80ee309d12cbb7f7e3f9cb9c95ecf163ed
Dzmitry Drevich <jeidan2012@gmail.com>
2025-08-11 01:49:09 +0200

    refactor: remove unused themeSlice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import selectedReducer from './selectedSlice';
-import themeReducer from './themeSlice';
 import apiSlice from '@/services/apiSlice';
 
 const store = configureStore({
   reducer: {
     selected: selectedReducer,
-    theme: themeReducer,
     [apiSlice.reducerPath]: apiSlice.reducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware)
