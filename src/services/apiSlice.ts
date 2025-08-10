3d9f3a981f3926ec07c3197c0148f9a3bed181fc
Dzmitry Drevich <jeidan2012@gmail.com>
2025-08-10 20:59:51 +0200

    feat: add RTK query apiSlice

---
+++ src/services/apiSlice.ts
@@ -0,0 +1,57 @@
+import { API_BASE } from '@/types/constants';
+import { type JikanApiResponse, type JikanApiResponseId } from '@/types/types';
+import getErrorMessage from '@/utils/http/getErrorMessage';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const apiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({
+    baseUrl: API_BASE,
+    validateStatus(response, body: unknown) {
+      if (response.ok) return true;
+
+      const serverMessage =
+        typeof body === 'object' && body !== null && 'message' in body
+          ? (body as { message?: string }).message
+          : undefined;
+
+      throw new Error(serverMessage || getErrorMessage(response.status));
+    }
+  }),
+  tagTypes: ['Anime'],
+  endpoints: (builder) => ({
+    getSeasonTop: builder.query<JikanApiResponse, number>({
+      query: (page) => `/top/anime?type=tv&filter=airing&page=${page}`,
+      providesTags: ['Anime']
+    }),
+    getAnimeSearch: builder.query<JikanApiResponse, { query: string; page?: number }>({
+      query: ({ query, page = 1 }) => `/anime?q=${encodeURIComponent(query)}&page=${page}`,
+      providesTags: ['Anime']
+    }),
+    getAnimeById: builder.query<JikanApiResponseId, number>({
+      query: (id) => `/anime/${id}`,
+      providesTags: (_result, _error, id) => [{ type: 'Anime', id }]
+    }),
+
+    // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+    refreshAnime: builder.mutation<undefined, void>({
+      queryFn: () => ({ data: undefined }),
+      invalidatesTags: ['Anime']
+    }),
+
+    refreshAnimeById: builder.mutation<undefined, number>({
+      queryFn: () => ({ data: undefined }),
+      invalidatesTags: (_result, _error, id) => [{ type: 'Anime', id }]
+    })
+  })
+});
+
+export const {
+  useGetSeasonTopQuery,
+  useGetAnimeSearchQuery,
+  useGetAnimeByIdQuery,
+  useRefreshAnimeMutation,
+  useRefreshAnimeByIdMutation
+} = apiSlice;
+
+export default apiSlice;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import selectedReducer from './selectedSlice';
 import themeReducer from './themeSlice';
+import apiSlice from '@/services/apiSlice';
 
 const store = configureStore({
   reducer: {
     selected: selectedReducer,
-    theme: themeReducer
-  }
+    theme: themeReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware)
 });
 
 export default store;
