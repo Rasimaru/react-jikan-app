28ee48ecddc5418f43e2a5d81fdceef2e301e24a
Dzmitry Drevich <jeidan2012@gmail.com>
2025-08-10 21:07:01 +0200

    feat: implement RTK query calls for MainPage

---
+++ src/components/results/RefreshButton.tsx
@@ -0,0 +1,21 @@
+import { RefreshCcw } from 'lucide-react';
+
+type RefreshButtonProps = {
+  onRefresh: () => void;
+  isRefreshing: boolean;
+};
+
+const RefreshButton = (props: RefreshButtonProps) => {
+  const { onRefresh, isRefreshing } = props;
+  return (
+    <button
+      disabled={isRefreshing}
+      onClick={onRefresh}
+      className="absolute sm:-top-15 -top-12.5 inline-flex justify-center items-center bg-amber-500 text-black py-2 px-3 hover:bg-amber-300 hover:cursor-pointer rounded duration-300 font-semibold text-base"
+    >
+      <RefreshCcw size={24} className="p-0.5 mr-1" />
+      Refresh
+    </button>
+  );
+};
+export default RefreshButton;
--- src/components/results/SearchResults.tsx
+++ src/components/results/SearchResults.tsx
@@ -6,16 +6,21 @@ import Spinner from '../shared/ui/Spinner';
 import CardList from './CardList';
 import Pagination from './Pagination';
 import DetailsDrawer from './DetailsDrawer';
+import RefreshButton from './RefreshButton';
+import { useRefreshAnimeMutation } from '@/services/apiSlice';
 
 const SearchResults = (props: ResultsProps & PaginationProps): JSX.Element => {
-  const { items, isLoading, error, searchQuery, page, totalPages, onPageChange } = props;
+  const { items, isLoading, isFetching, error, searchQuery, page, totalPages, onPageChange } =
+    props;
   const [searchParams] = useSearchParams();
   const detailsId = Number(searchParams.get('details')) || null;
 
   const isEmpty = items.length === 0;
 
-  if (isLoading) return <Spinner />;
+  const [refreshAnime, { isLoading: isRefreshing }] = useRefreshAnimeMutation();
 
+  if (isLoading) return <Spinner />;
+  if (isFetching && !isLoading) return <p className="text-lg">Reloading...</p>;
   if (error) return <p>{error}</p>;
 
   if (isEmpty)
@@ -26,10 +31,11 @@ const SearchResults = (props: ResultsProps & PaginationProps): JSX.Element => {
     );
 
   return (
-    <section aria-label="search results" className="flex flex-col gap-10 sm:pb-10 pb-5">
+    <section aria-label="search results" className="flex flex-col gap-10 sm:pb-10 pb-5 relative">
       <CardList items={items} />
       <Pagination page={page} totalPages={totalPages} onPageChange={onPageChange} />
-      {detailsId && <DetailsDrawer id={detailsId} />}
+      {detailsId && !isFetching && <DetailsDrawer id={detailsId} />}
+      <RefreshButton isRefreshing={isRefreshing} onRefresh={refreshAnime} />
     </section>
   );
 };
