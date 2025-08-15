77c93fd526c9780c9c889ada688f7e3ab8de54a3
Dzmitry Drevich <jeidan2012@gmail.com>
2025-08-16 00:51:21 +0200

    chore: add NextJS and base config

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  distDir: './dist',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH
+};
+
+export default nextConfig;
