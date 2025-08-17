1e32eecea6d196b9acfb377b99563eb87dcec39d
Dzmitry Drevich <jeidan2012@gmail.com>
2025-08-17 19:29:30 +0200

    chore: update project setup - remove deprecated & add new after Next.js migration

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: './dist',
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || ''
 };
 
 export default nextConfig;
